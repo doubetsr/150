5a301ead75f7d847a0cca9edad2546907e1cf4df
doubetsr <doubetsr@unk.edu>
2017-12-01 15:40:47 -0600

    Homework 4 with GUI in class

--- src/Homework4/MazeFrame.java
+++ src/Homework4/MazeFrame.java
@@ -1,30 +1,37 @@
-//package Homework4;
-//
-//import Lab9.ExerciseDisplay;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//import java.io.File;
-//
-//public class MazeFrame extends JFrame {
-//
-//    private File guiFile;
-//    private Maze guiMaze;
-//    private Robot guiRobot;
-//    private MazePanel guiPanel;
-//    private Boolean bCheck;
-//
-//public MazeFrame () {
-//    this.setTitle("Exercise Window");
-//    this.setSize( , );
-//    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    initializeWindow();
-//    this.setVisible(true);
-//    }
-//
-//    public void initializeWindow()
-//    {
-//        panel = new ExerciseDisplay();
-//        this.add(panel, BorderLayout.CENTER);
-//    }
-//}
+package Homework4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class MazeFrame extends JFrame {
+
+    private File guiFile;
+    private Maze guiMaze;
+    private Robot guiRobot;
+    private MazePanel guiPanel;
+    private Boolean bCheck;
+    JMenuItem fileOpenMenuItem;
+    JMenuItem fileSaveMenuItem;
+    JMenuItem robotMenuItem;
+    JMenuItem exitMenuItem;
+
+public MazeFrame () {
+    this.setTitle("Exercise Window");
+    this.setSize(1000,1000 );
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    initializeWindow();
+    this.setVisible(true);
+    }
+
+    public void initializeWindow()
+    {
+        guiPanel = new MazePanel();
+        this.add(guiPanel, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args)
+    {
+        MazeFrame ew = new MazeFrame();
+    }
+}
